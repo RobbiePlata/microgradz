7a9adeec495fbe29a527f9447233d750d13f3e6e
Robbie <norbertedguy@gmail.com>
2025-07-10 02:08:19 -0400

    chore: update to stable

--- src/main.zig
+++ src/main.zig
@@ -3,8 +3,7 @@ const Graph = @import("graph.zig").Graph;
 
 pub fn main() !void {
     std.debug.print("Run `zig build test --summary all` to execute unit tests.\n", .{});
-    const allocator = std.heap.page_allocator;
-    const g = Graph.init(allocator);
+    var g = Graph.init(std.heap.page_allocator);
     defer g.deinit();
 
     const a = g.value(1.0);
